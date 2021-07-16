38b3dcb333abb5d0faf161d2a4692dbc645abbde
Caio Cesar <kylespace@icloud.com>
2021-07-15 22:04:51 -0300

    finished main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ uint8_t array1[160] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 uint8_t unused2[64];
 uint8_t array2[256 * 512];
 
-char *secret = "Credit Card Password :D";
+char *secret = "Secret message :D";
 
 uint8_t temp = 0;
 
