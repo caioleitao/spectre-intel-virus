0d2232b1e2790793b6e26d7d8077a13233d4cafb
Caio Cesar <kylespace@icloud.com>
2021-07-15 21:32:07 -0300

    add main.cpp and libraries

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Kyle on 7/15/21.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#ifdef _MSC_VER
+#include <intrin.h>
+#pragma optimize("gt,on")
+#else
+#include <x86intrin.h>
+#endif
+
